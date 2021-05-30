c60de51474c036eb27716b6c7d3e19953d488a16
siddhesh-swami <siddhesh.swami98@gmail.com>
2021-05-30 12:04:14 +0530

    port changed

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ app.post("/", function(req,res) {
 
 })
 
-app.listen(3000, function() {
-	console.log("Server running on port 3000.");
+app.listen(process.env.PORT,function() {
+	console.log("Server running on port ${process.env.PORT}.");
 })
